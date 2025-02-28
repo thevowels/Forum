a6fee4f115f0de4b039c18284e8e2519e89a066b
thevowels <aungphyohtet1@gmail.com>
2025-03-01 06:12:48 +0700

    Adding eloquent relationships on the user/post/comment models.

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,9 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Comment extends Model
 {
     /** @use HasFactory<\Database\Factories\CommentFactory> */
     use HasFactory;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comment(): BelongsTo
+    {
+        return $this->belongsTo(Post::class);
+    }
 }
